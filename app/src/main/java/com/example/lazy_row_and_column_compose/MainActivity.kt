7eb23acc9a5231b4cdcff582cc0e4dced41eac16
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-25 21:57:00 +0530

    commit

--- app/src/main/java/com/example/lazy_row_and_column_compose/MainActivity.kt
+++ app/src/main/java/com/example/lazy_row_and_column_compose/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.lazy_row_and_column_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lazy_row_and_column_compose.ui.theme.Lazy_Row_and_Column_ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
                 }
             }
         }
@@ -30,17 +38,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainDisplay(){
+    Column(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            //Section 1
+
+            //Title
+            Text(
+                text = "Implementing Lazy Row",
+                fontSize = 25.sp,
+                textAlign = TextAlign.Justify,
+                modifier = Modifier.padding(16.dp)
+            )
+
+            //Lazy Row Code here
+        }
+
+        Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            //Section 2
+
+            //Title
+            Text(
+                text = "Implementing Lazy Column",
+                fontSize = 25.sp,
+                textAlign = TextAlign.Justify,
+                modifier = Modifier.padding(16.dp)
+            )
+
+            //Lazy Column Code Here
+            
+        }
+    }
+}
+@Composable
+fun LazyCard(){
+    Card() {
+        
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Lazy_Row_and_Column_ComposeTheme {
-        Greeting("Android")
+//        Greeting("Android")
+        MainDisplay()
     }
 }

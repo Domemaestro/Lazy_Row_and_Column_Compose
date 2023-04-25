4f8f017ade0fa94af0bdc9530e20030ff0460a05
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-25 22:13:40 +0530

    commit

--- app/src/main/java/com/example/lazy_row_and_column_compose/Data/DataSource.kt
+++ app/src/main/java/com/example/lazy_row_and_column_compose/Data/DataSource.kt
@@ -1,21 +1,27 @@
 package com.example.lazy_row_and_column_compose.Data
 
 import com.example.lazy_row_and_column_compose.Model.LazyData
+import com.example.lazy_row_and_column_compose.R
 
 class DataSource() {
     fun loadFun():List<LazyData>{
         return listOf<LazyData>(
             //to be done
-            LazyData("Test 1"),
-            LazyData("Test 2"),
-            LazyData("Test 3"),
-            LazyData("Test 4"),
-            LazyData("Test 5"),
-            LazyData("Test 6"),
-            LazyData("Test 7"),
-            LazyData("Test 8"),
-            LazyData("Test 9"),
-            LazyData("Test 10"),
+           LazyData(R.string.test1),
+            LazyData(R.string.test2),
+            LazyData(R.string.test3),
+            LazyData(R.string.test4),
+            LazyData(R.string.test5),
+            LazyData(R.string.test6),
+            LazyData(R.string.test7),
+            LazyData(R.string.test8),
+            LazyData(R.string.test9),
+            LazyData(R.string.test10),
+            LazyData(R.string.test11),
+            LazyData(R.string.test12),
+            LazyData(R.string.test13),
+            LazyData(R.string.test14),
+            LazyData(R.string.test15),
 
         )
     }
--- app/src/main/java/com/example/lazy_row_and_column_compose/MainActivity.kt
+++ app/src/main/java/com/example/lazy_row_and_column_compose/MainActivity.kt
@@ -3,21 +3,27 @@ package com.example.lazy_row_and_column_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.lazy_row_and_column_compose.Model.LazyData
 import com.example.lazy_row_and_column_compose.ui.theme.Lazy_Row_and_Column_ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -52,6 +58,7 @@ fun MainDisplay(){
             )
 
             //Lazy Row Code here
+
         }
 
         Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
@@ -71,9 +78,13 @@ fun MainDisplay(){
     }
 }
 @Composable
-fun LazyCard(){
-    Card() {
-        
+fun LazyCard(testText:LazyData){
+    Card(elevation = CardDefaults.cardElevation(20.dp), modifier = Modifier.padding(10.dp)) {
+        Box(modifier = Modifier
+            .width(150.dp)
+            .height(200.dp)) {
+            Text(text = stringResource(id = testText.StringData))
+        }
     }
 }
 
